aa1b86afa3452df6ce1ad5a81f2d526cf388039b
ksreyes <kenneth.rejes@gmail.com>
2025-11-27 22:29:19 +0100

    Change tab spacing

--- src/components/FilterLead.jsx
+++ src/components/FilterLead.jsx
@@ -1,41 +1,41 @@
 export default function FilterLead({ filters, setFilters }) {
 
-  const handleChange = (event) => {
-    setFilters((prevFilters) => ({
-      ...prevFilters,
-      lead: Number(event.target.value),
-    }));
-  };
+    const handleChange = (event) => {
+        setFilters((prevFilters) => ({
+            ...prevFilters,
+            lead: Number(event.target.value),
+        }));
+    };
 
-  let unit;
-  if (filters.indicator !== 2 && filters.lead === 1) {
-    unit = "month";
-  } else if (filters.indicator !== 2 && filters.lead !== 1) {
-    unit = "months";
-  } else if (filters.indicator === 2 && filters.lead === 1) {
-    unit = "day";
-  } else if (filters.indicator === 2 && filters.lead !== 1) {
-    unit = "days";
-  }
+    let unit;
+    if (filters.indicator !== 2 && filters.lead === 1) {
+        unit = "month";
+    } else if (filters.indicator !== 2 && filters.lead !== 1) {
+        unit = "months";
+    } else if (filters.indicator === 2 && filters.lead === 1) {
+        unit = "day";
+    } else if (filters.indicator === 2 && filters.lead !== 1) {
+        unit = "days";
+    }
 
-  const max = filters.indicator === 2 ? 29 : 6;
+    const max = filters.indicator === 2 ? 29 : 6;
 
-  return (
-    <div className="filter lead-filter">
-      <h2 htmlFor="lead-filter" className="filter-label block">
-        Lead: {filters.lead} {unit}
-      </h2>
+    return (
+        <div className="filter lead-filter">
+            <h2 htmlFor="lead-filter" className="filter-label block">
+                Lead: {filters.lead} {unit}
+            </h2>
 
-      <input
-        id="lead-filter"
-        type="range"
-        min="0"
-        max={max}
-        step="1"
-        value={filters.lead}
-        onChange={handleChange}
-        className="mgn-t-2"
-      />
-    </div>
-  );
+            <input
+                id="lead-filter"
+                type="range"
+                min="0"
+                max={max}
+                step="1"
+                value={filters.lead}
+                onChange={handleChange}
+                className="mgn-t-2"
+            />
+        </div>
+    );
 }
