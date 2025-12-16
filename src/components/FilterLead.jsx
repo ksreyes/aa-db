08faa86ed85c8f47ec4f3b98b78acf138dcf41cc
ksreyes <kenneth.rejes@gmail.com>
2025-12-16 16:38:18 +0100

    Update lead filter

--- src/components/FilterLead.jsx
+++ src/components/FilterLead.jsx
@@ -8,17 +8,26 @@ export default function FilterLead({ filters, setFilters }) {
     };
 
     let unit;
-    if (filters.indicator !== 2 && filters.lead === 1) {
-        unit = "month";
-    } else if (filters.indicator !== 2 && filters.lead !== 1) {
-        unit = "months";
-    } else if (filters.indicator === 2 && filters.lead === 1) {
+    if (filters.indicator !== 1 && filters.lead === 1) {
         unit = "day";
-    } else if (filters.indicator === 2 && filters.lead !== 1) {
+    } else if (filters.indicator !== 1 && filters.lead !== 1) {
         unit = "days";
+    } else if (filters.indicator === 1 && filters.lead === 1) {
+        unit = "month";
+    } else if (filters.indicator === 1 && filters.lead !== 1) {
+        unit = "months";
     }
 
-    const max = filters.indicator === 2 ? 29 : 6;
+    let max;
+    if (filters.indicator === 1 ) {
+        max = 5;
+    } else if (filters.indicator === 2 ) {
+        max = 29;
+    } else if (filters.indicator === 3 ) {
+        max = 10;
+    } else if (filters.indicator === 4 ) {
+        max = 7;
+    }
 
     return (
         <div className="filter lead-filter">
